3d7fda796b6e2eac25378a7a0d3c4515a5df32e8
RuthMeryCardenas <ruth.cardenas.perez@gmail.com>
2017-07-05 17:36:24 -0500

    Probando plantilla de modal

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,27 @@
-console.log("cargar app.js");
+'use strict';
+console.log("cargando... app.js");
+
+const state = {
+  board        : null,
+  pins         : null,
+  current_pin  : "523473156678861702"
+};
+
+// const render = (root) => {
+//   root.empty();
+//   const wrapper = $('<div class="wrapper"></div>');
+//
+//   wrapper.append(Pin());
+//   root.append(wrapper);
+// }
+
+$( _ => {
+
+  getJSON(generate_url("pin",state.current_pin), (err, json) => {
+    if (err) { return alert(err.message);}
+    state.current_pin = json.data;
+    console.log(state.current_pin);
+    // const root = $('.root');
+    // render(root);
+  });
+});
