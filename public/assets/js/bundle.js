40dbc8c52365c2c8448dc437df6a32e6bc16150d
Katherine Ortega <katy_ja2915@hotmail.com>
2017-07-05 23:44:22 -0500

    getJSON con jquery

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -54,25 +54,32 @@ const get = (url, callback) => {
 
 console.log('cargar board');
 
-
 //COMPONENT HEADER
 
-// const Header = () => {
-//     const nav = $('<nav class="navbar navbar-default navbar-fixed-top"></nav>');
-//     const container = $('<div class="container"></div>');
-//     const p = $('<p>Hola!!</p>');
-//
-//     container.append(p);
-//     nav.append(container);
-//
-//     return nav;
-// };
+const Header = () => {
+    const nav = $('<nav class="navbar navbar-default navbar-fixed-top"></nav>');
+    const container = $('<div class="container"></div>');
+    const p = $('<p>Hola!!</p>');
+
+    container.append(p);
+    nav.append(container);
+
+    return nav;
+};
 
 console.log('cargar modal');
 
 console.log('cargar lista de pines');
 
 'use strict';
+
+const render = (root) => {
+    root.empty();
+    const wrapper = $('<div class="wrapper"></div>');
+    wrapper.append(Header());
+
+    root.append(wrapper);
+};
 
 const state = {
     user: null,
@@ -82,34 +89,6 @@ const state = {
     current_pin: "AZvD2sZHc13NAEh_yzBNFXT2gG8Ev5dHlBFLxorpuq7RIFTL66qnsJI"
 };
 
-    /*getJSON(generate_url("creator-board"), (err, json) => {
-        if (err) {
-            return alert(err.message);
-        }
-        state.creator_board = json.data;
-        console.log(state.creator_board);
-    });
-    getJSON(generate_url("board", ""), (err, json) => {
-        if (err) {
-            return alert(err.message);
-        }
-        state.board = json.data;
-        console.log(state.board);
-    });
-    getJSON(generate_url("pin-list", ""), (err, json) => {
-        if (err) {
-            return alert(err.message);
-        }
-        state.pins = json.data;
-        console.log(state.pins);
-    });
-    getJSON(generate_url("pin", state.current_pin), (err, json) => {
-        if (err) {
-            return alert(err.message);
-        }
-        state.current_pin = json.data;
-        console.log(state.current_pin);
-    });*/
 $( _ => {
 
     get(urlGenerate('creator-board'),(err,data) => {
@@ -126,6 +105,10 @@ $( _ => {
     get(urlGenerate('pin'),(err,data) => {
         console.log(data);
     });
+
+    const root = $('.root');
+
+    render(root);
 
 });
 
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const render = (root) => {
+    root.empty();
+    const wrapper = $('<div class="wrapper"></div>');
+    wrapper.append(Header());
+
+    root.append(wrapper);
+};
+
 const state = {
     user: null,
     creator_board: null,
@@ -8,34 +16,6 @@ const state = {
     current_pin: "AZvD2sZHc13NAEh_yzBNFXT2gG8Ev5dHlBFLxorpuq7RIFTL66qnsJI"
 };
 
-    /*getJSON(generate_url("creator-board"), (err, json) => {
-        if (err) {
-            return alert(err.message);
-        }
-        state.creator_board = json.data;
-        console.log(state.creator_board);
-    });
-    getJSON(generate_url("board", ""), (err, json) => {
-        if (err) {
-            return alert(err.message);
-        }
-        state.board = json.data;
-        console.log(state.board);
-    });
-    getJSON(generate_url("pin-list", ""), (err, json) => {
-        if (err) {
-            return alert(err.message);
-        }
-        state.pins = json.data;
-        console.log(state.pins);
-    });
-    getJSON(generate_url("pin", state.current_pin), (err, json) => {
-        if (err) {
-            return alert(err.message);
-        }
-        state.current_pin = json.data;
-        console.log(state.current_pin);
-    });*/
 $( _ => {
 
     get(urlGenerate('creator-board'),(err,data) => {
@@ -52,5 +32,9 @@ $( _ => {
     get(urlGenerate('pin'),(err,data) => {
         console.log(data);
     });
+
+    const root = $('.root');
+
+    render(root);
 
 });
