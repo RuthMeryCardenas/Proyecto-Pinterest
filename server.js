7bb3e4a0e4fb5a03841398e9b237eea31bd7268d
Katherine Ortega <katy_ja2915@hotmail.com>
2017-07-06 13:01:02 -0500

    arreglando readme

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use('/', express.static('public/'));
 
-app.listen(3000, () => {
-	console.log("Listening on 3000");
+app.listen(8000, () => {
+	console.log("Listening on 8000");
 });
