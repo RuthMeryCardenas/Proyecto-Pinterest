1ee0b901971c157fce3a881d8c839ff06f200d8c
Katherine Ortega <katy_ja2915@hotmail.com>
2017-07-05 23:33:29 -0500

    concat terminado

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,33 +1,56 @@
 'use strict';
-console.log("cargando... app.js");
 
 const state = {
-  user         : null,
-  creator_board: null,
-  board        : null,
-  pins         : null,
-  current_pin  : "AZvD2sZHc13NAEh_yzBNFXT2gG8Ev5dHlBFLxorpuq7RIFTL66qnsJI"
+    user: null,
+    creator_board: null,
+    board: null,
+    pins: null,
+    current_pin: "AZvD2sZHc13NAEh_yzBNFXT2gG8Ev5dHlBFLxorpuq7RIFTL66qnsJI"
 };
 
+    /*getJSON(generate_url("creator-board"), (err, json) => {
+        if (err) {
+            return alert(err.message);
+        }
+        state.creator_board = json.data;
+        console.log(state.creator_board);
+    });
+    getJSON(generate_url("board", ""), (err, json) => {
+        if (err) {
+            return alert(err.message);
+        }
+        state.board = json.data;
+        console.log(state.board);
+    });
+    getJSON(generate_url("pin-list", ""), (err, json) => {
+        if (err) {
+            return alert(err.message);
+        }
+        state.pins = json.data;
+        console.log(state.pins);
+    });
+    getJSON(generate_url("pin", state.current_pin), (err, json) => {
+        if (err) {
+            return alert(err.message);
+        }
+        state.current_pin = json.data;
+        console.log(state.current_pin);
+    });*/
 $( _ => {
-  getJSON(generate_url("creator-board","arabelyuska"), (err, json) => {
-    if (err) { return alert(err.message);}
-    state.creator_board = json.data;
-    console.log(state.creator_board);
-  });
-  getJSON(generate_url("board","web-ui"), (err, json) => {
-    if (err) { return alert(err.message);}
-    state.board = json.data;
-    console.log(state.board);
-  });
-  getJSON(generate_url("pin-list","web-ui"), (err, json) => {
-    if (err) { return alert(err.message);}
-    state.pins = json.data;
-    console.log(state.pins);
-  });
-  getJSON(generate_url("pin",state.current_pin), (err, json) => {
-    if (err) { return alert(err.message);}
-    state.current_pin = json.data;
-    console.log(state.current_pin);
-  });
+
+    get(urlGenerate('creator-board'),(err,data) => {
+        //if (err) console.log(err);
+        console.log(data);
+    });
+    get(urlGenerate('pin-list'),(err,data) => {
+        console.log(data);
+    });
+
+    get(urlGenerate('board'),(err,data) => {
+       console.log(data);
+    });
+    get(urlGenerate('pin'),(err,data) => {
+        console.log(data);
+    });
+
 });
--- src/assets/js/utils/getJSON.js
+++ src/assets/js/utils/getJSON.js
@@ -1,58 +1,45 @@
-'use strict';
-const generate_url = (component, reference) => {
-  const token      =  "/?access_token=AdZr116hPQFc_5cV-0_N3MTDskptFM6A_fkl_oFEIux5zaBB9QAAAAA";
-  const static_url = "https://api.pinterest.com/v1/";
-  let data         = null;
-  let url          = null;
 
-  switch (component) {
-    case "creator-board":
-      data = "&fields=first_name%2Cid%2Clast_name%2Curl%2Cbio%2Ccounts%2Cusername%2Cimage";
-      url  = static_url + "users/" + reference + token + data;
-      console.log("generando user");
-      console.log(url);
-      break;
-    case "board":
-      data = "&fields=id%2Cname%2Curl%2Ccounts%2Ccreated_at%2Cprivacy%2Creason%2Ccreator%2Cdescription%2Cimage";
-      url  = static_url + "boards/" + "arabelyuska" + "/" + reference + token + data;
-      console.log("generando url de board");
-      console.log(url);
-      break;
-    case "pin-list":
-      data = "&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Ccolor%2Cmedia%2Cattribution%2Coriginal_link%2Cmetadata%2Cboard%2Ccounts%2Ccreated_at%2Ccreator";
-      url  = static_url + "boards/" + "arabelyuska" + "/" + reference + "/pins" + token + data;
-      console.log("generando url de lista de pines");
-      break;
-    case "pin":
-      data = "&fields=id%2Clink%2Cnote%2Curl%2Cattribution%2Cboard%2Ccolor%2Cmetadata%2Cmedia%2Ccounts%2Ccreated_at%2Coriginal_link%2Ccreator%2Cimage";
-      url  = static_url + "pins/" + reference + token + data;
-      console.log("generando url de pin");
-      console.log(url);
-      break;
-    default:
-  }
-  return url;
-}
+//GETJSON
 
-const getJSON = (url, cb) => {
+const urlGenerate = (component) => {
+    const staticUrl = 'https://api.pinterest.com/v1/';
+    const token = "/?access_token=AdZr116hPQFc_5cV-0_N3MTDskptFM6A_fkl_oFEIux5zaBB9QAAAAA";
+    let boardCreator = "arabelyuska";
+    let boardName = "web-ui";
+    let data = null;
+    let url = null;
 
-  const xhr = new XMLHttpRequest();
-
-  xhr.addEventListener('load', () => {
-
-    if (xhr.status !== 200) {
-      return cb(new Error('Error loading JSON from ' + url + '(' + xhr.status + ')'));
+    switch (component) {
+        case "creator-board":
+            data = "&fields=first_name%2Cid%2Clast_name%2Curl%2Cbio%2Ccounts%2Cusername%2Cimage";
+            url = "users/" + boardCreator + token + data;
+            console.log("generando user");
+            break;
+        case "board":
+            data = "&fields=id%2Cname%2Curl%2Ccounts%2Ccreated_at%2Cprivacy%2Creason%2Ccreator%2Cdescription%2Cimage";
+            url = "boards/" + boardCreator + '/' + boardName + token + data;
+            console.log("generando url de board");
+            break;
+        case "pin-list":
+            data = "&fields=id%2Clink%2Cnote%2Curl%2Cimage%2Ccolor%2Cmedia%2Cattribution%2Coriginal_link%2Cmetadata%2Cboard%2Ccounts%2Ccreated_at%2Ccreator";
+            url =  "boards/" + boardCreator + '/' + boardName + "/pins" + token + data;
+            console.log("generando url de lista de pines");
+            break;
+        case "pin":
+            data = "&fields=id%2Clink%2Cnote%2Curl%2Cattribution%2Cboard%2Ccolor%2Cmetadata%2Cmedia%2Ccounts%2Ccreated_at%2Coriginal_link%2Ccreator%2Cimage";
+            url =  "pins/" + state.current_pin + token + data;
+            console.log("generando url de pin");
+            break;
+        default:
     }
-
-    cb(null, xhr.response);
-  });
-
-  xhr.open('GET', url);
-  xhr.responseType = 'json';
-  xhr.send();
-
- //  $.get(url,(data) => {
- //   if (data.status != 200) callback(new Error("Error al obtener la data"));
- //   callback(null,data);
- // });
+    return staticUrl + url;
 };
+
+const get = (url, callback) => {
+    $.getJSON(url, (data) => {
+        //if (data.status !== 200) return callback(new Error("Error al obtener la data"));
+        callback(null, data);
+    });
+};
+
+//END GETJSON
